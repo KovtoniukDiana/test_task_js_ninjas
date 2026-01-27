b2ed729b8ffb06de2a4a10ad27877a0c942073fa
KovtoniukDiana <buben111004@gmail.com>
2026-01-27 21:31:39 +0200

    fix: convert client submodule to regular folder

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    <div className="w-full bg-red-700 h-full">
+      <header className='flex justify-center items-center'>
+        <p>Список супергероїв</p>
+      </header>
+
+    
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/adding_panel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddingPanel() {
+  return (
+    <div classname=''>
+
+        
+    </div>
+  )
+}
